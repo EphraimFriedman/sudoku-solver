4ae81b5b4cb6313e2f738704310ba8907ce38647
EphraimFriedman <ephraimfried@gmail.com>
2016-11-10 15:39:56 -0500

    refactor cheacking results

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,31 +1,34 @@
 class Puzzle < ApplicationRecord
 
-	
+
+
 def solve_puzzle
 
   # new_board = str_to_array
-  
+	@board = self.board
   running_board = epmty_cell_to_posibilities(str_to_array)
 
   until solved?(running_board)
 
     running_board.map do |row|
       row.map!.with_index do |cell, index|
-      	
+
         if cell.length > 1
           # check cell against row
-          cell = checker(row, cell)
+          # cell = checker(row, cell)
+					#
+          # # get column
+          # column = get_column(running_board, index)
+					#
+          # # check cell against column
+          # cell = checker(column, cell)
+          # # get box
+					#
+          # box = get_box(running_board, running_board.index(row), index)
+          # # check cell against box
+          # cell = checker(box, cell)
 
-          # get column
-          column = get_column(running_board, index)
-          
-          # check cell against column
-          cell = checker(column, cell)
-          # get box
-
-          box = get_box(running_board, running_board.index(row), index)
-          # check cell against box
-          cell = checker(box, cell)
+					cell = cell - get_row_column_box(running_board, running_board.index(row), index )
 
           ## advanced logic for puzzles 5- 10
             # # IF the cell length is greater than 1
@@ -47,7 +50,7 @@ def solve_puzzle
       end
     end
   end
-  
+
   running_board.flatten
 end
 
@@ -81,43 +84,50 @@ def epmty_cell_to_posibilities(board)
   board.map do |row|
     row.map! do |cell|
       if cell == "-"
-        cell = ("1".."9").to_a 
+        cell = ("1".."9").to_a
       else
         cell
       end
     end
-  end  
+  end
 end
 
+def get_row_column_box(grid, row_index, cell_index)
+	get_row(grid, row_index) + get_column(grid, cell_index) + get_box(grid, row_index, cell_index)
 
+end
+
+def get_row(grid, index)
+	grid[index].reject {|num| num.is_a?(Array)}
+end
 
 def get_column(grid, cell_index)
   grid.map do |row|
-    row[cell_index] unless row[cell_index].is_a?(Array) 
-  end  
+    row[cell_index] unless row[cell_index].is_a?(Array)
+  end
  # Another way I played around with
  # grid.transpose[cell_index].reject {|num| num.is_a?(Array)}
 end
 
 
-def checker(single_array, cell_posibilities)
-  new_posibilities = []
-  cell_posibilities.map do |num|
-    if single_array.include?(num) == false
-      new_posibilities << num
-    end
-  end
-  cell_posibilities = new_posibilities
-end
+# def checker(single_array, cell_posibilities)
+#   new_posibilities = []
+#   cell_posibilities.map do |num|
+#     if single_array.include?(num) == false
+#       new_posibilities << num
+#     end
+#   end
+#   cell_posibilities = new_posibilities
+# end
 
 def get_box(grid, cell_row, cell_index)
-  #Get the first cell in the box, x and y coordinates 
+  #Get the first cell in the box, x and y coordinates
   x = cell_row / 3 * 3
   y = cell_index / 3 * 3
   box = []
-  
+
   # Get the 3 filled cells from every row in the box
-  3.times do 
+  3.times do
     box << grid[x][y.. (y + 2)].reject {|num| num.is_a?(Array)}
     x += 1
   end
