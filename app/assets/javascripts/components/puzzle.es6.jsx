6e3f104fe6a117d4ddeee3dda8898ab531dd3885
EphraimFriedman <ephraimfried@gmail.com>
2016-11-10 18:05:28 -0500

    add display toggle for buttons

--- app/assets/javascripts/components/puzzle.es6.jsx
+++ app/assets/javascripts/components/puzzle.es6.jsx
@@ -2,10 +2,14 @@ class Puzzle extends React.Component{
 	constructor(){
 		super()
 		this.state = {
-			board: []
+			id: "",
+			board: [],
+			displayPuzzleButton: false,
 		}
 		this.split_board = this.split_board.bind(this),
-		this.solve = this.solve.bind(this)
+		this.solve = this.solve.bind(this),
+		this.newPuzzle = this.newPuzzle.bind(this),
+		this.buttonDisplay = this.buttonDisplay.bind(this)
 	}
 
 	componentDidMount(){
@@ -13,7 +17,9 @@ class Puzzle extends React.Component{
 		new_board = this.split_board(this.props.data.board)
 
 		this.setState({
+			id: this.props.data.id,
 			board: new_board
+
 		})
 	}
 
@@ -32,15 +38,50 @@ class Puzzle extends React.Component{
 		event.preventDefault()
 
 		id = this.props.data.id
+
+		this.setState({
+			id: id
+		})
+
 		$.ajax({
-			url: "/solve/" + this.props.data.id,
+			url: "/solve/" + this.state.id,
 			method: 'GET'
 			}).done((response) =>{
 
 				this.setState({
-			board: this.split_board(response)
+			board: this.split_board(response),
+			displayPuzzleButton: true
 		})
 			})
+	}
+
+	newPuzzle(event){
+		event.preventDefault()
+
+		$.ajax({
+			url: "/puzzles",
+			method: 'GET'
+			}).done((response) =>{
+
+			this.setState({
+				id: response.id,
+				board: this.split_board(response.board),
+				displayPuzzleButton: false
+			})
+
+			})
+	}
+
+	toggleNewPuzzleButton(){
+
+	}
+
+	buttonDisplay(){
+		if(this.state.displayPuzzleButton){
+			return(<button type="button" className="btn-lg btn-success btn-block" onClick={this.newPuzzle}>New Puzzle!</button>)
+		}else{
+			return(<button type="button" className="btn-lg btn-success btn-block" onClick={this.solve}>Solve!</button>)
+		}
 	}
 
 	render(){
@@ -77,7 +118,8 @@ class Puzzle extends React.Component{
 				</div>
 				<div className="row">
 					<div className="col-md-4 col-md-offset-4">
-						<button type="button" className="btn-lg btn-success btn-block" onClick={this.solve}>Solve!</button>
+					{this.buttonDisplay()}
+						
 					</div>
 				</div>
 			 </div>
--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,14 +1,18 @@
 class PuzzlesController < ApplicationController
 
 	def index
-		@puzzle = Puzzle.first(5).sample
-
+			@puzzle = Puzzle.first(5).sample
+		if request.xhr?
+			render json: @puzzle
+		else
+			@puzzle
+		end
 	end
 
 	def solve
 		@puzzle = Puzzle.find(params[:puzzle_id])
 		@solved_board = @puzzle.solve_puzzle
-	
+
 		render text: @solved_board
 	end
 end
